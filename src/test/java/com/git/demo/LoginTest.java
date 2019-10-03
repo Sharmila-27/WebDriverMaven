53f6c3d8ae0b4a795c2b40849af675de1574bdec
Sharmila <mailsharmiarao@gmail.com>
2019-10-03 14:34:06 -0500

    My initial commit

---
+++ src/test/java/com/git/demo/LoginTest.java
@@ -0,0 +1,36 @@
+package com.git.demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+		
+		System.setProperty("webdriver.gecko.driver", "c:/drivers/geckodriver.exe");
+		driver = new FirefoxDriver();
+	}
+	
+	@Test
+	public void doLogin() {
+		
+		driver.get("https://gmail.com");
+		driver.findElement(By.name("identifier")).sendKeys("mailsharmilarao@gmail.com");
+		driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/span/span")).click();
+		driver.findElement(By.xpath("//input[@class='whsOnd zHQkBf']"));	
+		
+	}
+	@AfterSuite
+	public void tearDown() {
+		
+		driver.quit();
+	}
+
+}

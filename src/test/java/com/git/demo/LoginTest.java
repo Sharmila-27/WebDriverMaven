7a123d21759d886875211f21f966115c09035c72
Sharmila <mailsharmiarao@gmail.com>
2019-10-06 08:28:21 -0500

    sleep and wait method added to Login

--- src/test/java/com/git/demo/LoginTest.java
+++ src/test/java/com/git/demo/LoginTest.java
@@ -1,8 +1,13 @@
 package com.git.demo;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
@@ -19,13 +24,19 @@ public class LoginTest {
 	}
 	
 	@Test
-	public void doLogin() {
+	public void doLogin() throws InterruptedException {
+		driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
+		WebDriverWait wait = new WebDriverWait(driver,20);
 		
 		driver.get("https://gmail.com");
 		driver.findElement(By.name("identifier")).sendKeys("mailsharmilarao@gmail.com");
 		driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/span/span")).click();
-		driver.findElement(By.xpath("//input[@class='whsOnd zHQkBf']"));	
-		driver.findElement(By.xpath("//span[@class='CwaK9']")).click();
+		
+		driver.findElement(By.xpath("//input[@class='whsOnd zHQkBf']")).sendKeys("pasas");
+		Thread.sleep(2000);
+		WebElement clickOnNext = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='RveJvd snByac']")));
+		//driver.findElement(By.xpath("//span[@class='RveJvd snByac']")).click();
+		clickOnNext.click();
 	}
 	@AfterSuite
 	public void tearDown() {
